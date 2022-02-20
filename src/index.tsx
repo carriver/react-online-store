ee6f7619a70b9a9d8981eb760b0d647ea037ea64
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-02-20 06:31:01 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import App from "./App";
+
+const client = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
